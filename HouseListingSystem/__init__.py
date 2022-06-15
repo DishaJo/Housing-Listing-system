eb65ee9a4d91e9871686fd1ab6c26db5edce001a
Disha Joshi <dishaj.inexture@gmail.com>
2022-06-15 16:46:46 +0530

    set config variables

--- HouseListingSystem/__init__.py
+++ HouseListingSystem/__init__.py
@@ -18,8 +18,8 @@ login_manager = LoginManager(app)
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.getenv('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.getenv('EMAIL_PASSWORD')
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASSWORD')
 mail = Mail(app)
 
 from HouseListingSystem import routes
